eaff6d73299c1cfa7287c145282208d8e40008dd
NelsonJfsg <nelin@DESKTOP-I3QOV30>
2020-10-19 17:49:24 -0700

    Agregar archivos de proyecto.

---
+++ Operaciones/Operacionesbasicas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    public class Operacionesbasicas
+    {
+        public static Double suma(double num0, double num1)
+        {
+            return (num0 + num1);
+        }
+
+        public static Double resta(double num0, double num1)
+        {
+            return (num0 - num1);
+        }
+        public static Double mult(double num0, double num1)
+        {
+            return (num0 * num1);
+        }
+        public static Double div(double num0, double num1)
+        {
+            return (num0 / num1);
+        }
+    }
+}
